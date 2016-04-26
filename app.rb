23df0fd0fe70b65a7554b4c9e19875c0124bb672
axl812 <axl812@gmail.com>
2016-04-26 20:47:27 +0300

    keep values on post

--- app.rb
+++ app.rb
@@ -35,16 +35,17 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
 	
-	c = Client.new params[:client]
-	if c.save # .save принимает значение false если пустой
+	@c = Client.new params[:client]
+	if @c.save # .save принимает значение false если пустой
 		erb "<h2>Спасибо, вы записались!</h2>"
 	else
-		@error = c.errors.full_messages.first
+		@error = @c.errors.full_messages.first
 		erb :visit
 	end
 end
